41304e6e81622b5fdd3620ae80c3b2ae984f8ea1
darkcorp <darkcorpd@gmail.com>
2022-07-13 21:46:06 +0300

    init: brackets task start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const reducedBracketsConfig = bracketsConfig.map((bracket) => {
+    return `${bracket[0]}${bracket[1]}`;
+  });
+
+  let initialLength = str.length;
+
+  while (str.length) {
+    reducedBracketsConfig.forEach((bracket) => {
+      str = str.replaceAll(bracket, '');
+    });
+    if (str.length === initialLength) {
+      return false;
+    }
+    initialLength = str.length;
+  }
+  return true;
 }
